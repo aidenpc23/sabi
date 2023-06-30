8d0405beccbb1ea9b9afb6b13cee75814814e92c
aidenpc23 <aidenpc23@gmail.com>
2023-06-30 10:55:06 -0400

    Implemented basic functionality.

--- sabir/src/main.rs
+++ sabir/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpStream;
+use tokio::io::AsyncWriteExt;
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Usage: sender <host:port> <message>");
+        return Ok(());
+    }
+
+    let mut stream = TcpStream::connect(&args[1]).await?;
+    stream.write_all(args[2].as_bytes()).await?;
+    println!("Message sent");
+
+    Ok(())
 }
