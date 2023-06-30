cc309e2b13ce040afeaf5195c4eb114da4ceeff2
aidenpc23 <aidenpc23@gmail.com>
2023-06-30 11:04:01 -0400

    Fix overwite glitch thing

--- sabir/src/main.rs
+++ sabir/src/main.rs
@@ -1,19 +1,32 @@
-use tokio::net::TcpStream;
-use tokio::io::AsyncWriteExt;
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, Error};
 use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-
-    if args.len() != 3 {
-        eprintln!("Usage: sender <host:port> <message>");
+    
+    if args.len() != 2 {
+        eprintln!("Usage: receiver <port>");
         return Ok(());
     }
 
-    let mut stream = TcpStream::connect(&args[1]).await?;
-    stream.write_all(args[2].as_bytes()).await?;
-    println!("Message sent");
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", args[1])).await?;
+    println!("Listening on: {}", args[1]);
 
-    Ok(())
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        let mut buf = vec![0; 1024];
+        match socket.read(&mut buf).await {
+            Ok(_n) => {
+                let message = String::from_utf8_lossy(&buf);
+                println!("Message received: {}", message.trim_matches(char::from(0)));
+            }
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                continue;
+            }
+            Err(e) => return Err(e.into()),
+        }
+    }
 }
